341bda19919b6e4f45fcbba15ea83438fb71f85b
George Charles <gkcharles@outlook.com>
2021-02-27 09:18:46 +0100

    add server.js

--- server.js
+++ server.js
@@ -1,61 +1,16 @@
-const path = require("path");
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const colors = require("colors");
-const cookieParser = require("cookie-parser");
-const errorHandler = require("./middleware/error");
-const connectDB = require("./config/db");
+const fs = require("fs");
+const https = require("https");
 
-// Load env variables
-dotenv.config({
-  path: "./config/config.env",
+const options = {
+  key: fs.readFileSync("keys/server-key.pem"),
+
+  cert: fs.readFileSync("keys/server-cert.pem"),
+};
+
+const server = https.createServer(options, function (req, res) {
+  res.end("Hello World");
 });
 
-// connect to database
-connectDB();
-
-// Routes files
-const todos = require("./routes/todos");
-const auth = require("./routes/auth");
-const users = require("./routes/users");
-
-const app = express();
-
-// Body parser
-app.use(express.json());
-
-// Cookie parser
-app.use(cookieParser());
-
-// Dev Logging middleware
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-
-// Set static folder
-app.use(express.static(path.join(__dirname, "public")));
-
-// Mount routers
-app.use("/api/v1/todos", todos);
-app.use("/api/v1/auth", auth);
-app.use("/api/v1/users", users);
-
-// Error Handlers
-app.use(errorHandler);
-
-PORT = process.env.PORT || 5008;
-
-const server = app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-);
-
-// Handle unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
-  // Close server & exit process
-  // server.close(() => process.exit(1));
+server.listen(5000, function () {
+  console.log("server listening on port 5000");
 });
